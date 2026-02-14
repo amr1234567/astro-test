6a216a345ccd61b6951f84002191543505bdee0b
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2026-02-14 13:53:39 +0200

    first commit

---
+++ src/content/blog/first-post.md
@@ -0,0 +1,11 @@
+---
+title: "أول مقال تجريبي"
+pubDate: 2024-02-14
+description: "ده مجرد تيست عشان نشوف الدنيا شغالة ولا لأ."
+author: "Amr Shalaby"
+image:
+    url: "https://docs.astro.build/assets/rose.webp"
+    alt: "The Astro logo on a dark background with a pink glow."
+---
+# مرحبًا بالعالم
+ده أول مقال مكتوب بـ Markdown داخل Astro.
---
+++ src/content/config.js
@@ -0,0 +1,19 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional()
+  })
+});
+
+export const collections = {
+  blog: blogCollection,
+};
